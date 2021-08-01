d6c162464d20349d1e940933e51f6b37e850f2c6
jerry-ye-xu <jerryxu2500@mgail.com>
2021-08-01 04:32:15 +0000

    used Valgrind to successfully debug the issue. tldr; malloc() sizeof() was incorrect, needed memset and care with initialising struct Node inside function

--- C-revision/wk3/stack-implementation/tests.c
+++ C-revision/wk3/stack-implementation/tests.c
@@ -13,30 +13,26 @@ int max_(int x, int y) {
 }
 
 int test_empty_stack_peek() {
-    struct Stack* stack = (struct Stack*) malloc(sizeof(struct Stack*));
-    // if(stack) { printf("stack should be empty: %p\n", (void *) stack); }
+    struct Stack* stack = malloc(sizeof(struct Stack));
+    memset(stack, 0, sizeof(struct Stack));
     stack->size = 0;
-    void *top = peek(stack);
 
+    void *top = peek(stack);
     int result = *(int*)top;
+
     free(top);
-    // if(stack) { printf("stack should be empty: %p\n", (void *) stack); }
     free(stack);
-    // if(stack) { printf("stack should be empty: %p\n", (void *) stack); }
     return result == -1 ? 1 : 0;
 
 }
 
 int test_push_into_empty_stack() {
-    struct Stack* stack = NULL;
-    if(!stack) { printf("stack should be empty: %p\n", (void *) stack); }
-    stack = (struct Stack*) malloc(sizeof(struct Stack*));
-    if(stack) { printf("stack should be empty: %p\n", (void *) stack); }
-    stack->size = 0;
     int data = 50;
     int *d_ptr = &data;
-    // printf("sizeof(*data): %lu\n", sizeof(data));
-    // printf("sizeof(*d_ptr): %lu\n", sizeof(d_ptr));
+    struct Stack* stack = malloc(sizeof(struct Stack));
+    memset(stack, 0, sizeof(struct Stack));
+    stack->size = 0;
+
     push(stack, d_ptr, sizeof(d_ptr));
     void *top = peek(stack);
 
@@ -45,17 +41,16 @@ int test_push_into_empty_stack() {
 
     size_t size = stack->size;
     int result = *(int *)((struct Node*)top)->val;
+
     free(stack->top);
     free(stack);
-    stack->top = NULL;
-    stack = NULL;
-    // if(stack) { printf("stack should be empty: %p\n", (void *) stack); }
     return result == 50 && size ? 1 : 0;
 }
 
 int test_push_into_non_empty_stack() {
-    struct Stack* stack = (struct Stack*) malloc(sizeof(struct Stack*));
-    
+    struct Stack* stack = (struct Stack*) malloc(sizeof(struct Stack));
+    memset(stack, 0, sizeof(struct Stack));
+
     // Add first element
     stack->size = 0;
     int data = 1;
@@ -68,38 +63,67 @@ int test_push_into_non_empty_stack() {
     char *t_ptr = text;
     push(stack, t_ptr, sizeof(t_ptr));
     void *top = peek(stack);
-    
+
     size_t size = stack->size;
     char* text_data = (char *)((struct Node*)top)->val;
     printf("text_data: %s\n", text_data);
 
     int expected = strcmp(text_data, text);
     printf("%d\n", expected);
+
+    free_stack(stack);
+
     return !expected && size == 2 ? 1 : 0;
 
 }
 
+int test_push_into_non_empty_stack_then_pop() {
+    struct Node* node_pop;
+    struct Stack* stack = (struct Stack*) malloc(sizeof(struct Stack));
+    memset(stack, 0, sizeof(struct Stack));
+
+    // Add first element
+    stack->size = 0;
+    int data = 1;
+    int *d_ptr = &data;
+    push(stack, d_ptr, sizeof(d_ptr));
+
+    // Add second element
+    char text[6] = "hello";
+    char *t_ptr = text;
+    push(stack, t_ptr, sizeof(t_ptr));
+
+    // Pop and test
+    node_pop = (struct Node *) pop(stack);
+    char* text_pop = (char *) node_pop->val;
+    int expected = strcmp(text_pop, text);
+    int size = (int) stack->size == 1;
+
+    free(node_pop);
+    free_stack(stack);
+
+    return !expected && size == 1 ? 1 : 0;
+
+}
+
 int main(int argc, char** argv) {
-    // struct Stack* stack = (struct Stack*) malloc(sizeof(struct Stack*));
-    // stack->size = 0;
-    // void *top = peek(stack);
-
-    // printf("top %d\n", *(int*)top);
-
     int res;
     int pass = 1;
     res = test_empty_stack_peek();
     printf("| %d | test_empty_stack_peek\n", pass);
     pass = min_(res, pass);
-    
+
     res = test_push_into_empty_stack();
     printf("| %d | test_push_into_empty_stack\n", pass);
     pass = min_(res, pass);
-    
+
     res = test_push_into_non_empty_stack();
     printf("| %d | test_push_into_non_empty_stack\n", pass);
     pass = min_(res, pass);
 
-    // free(stack);
+    res = test_push_into_non_empty_stack_then_pop();
+    printf("| %d | test_push_into_non_empty_stack_then_pop\n", pass);
+    pass = min_(res, pass);
+
     return pass ? 0 : 1;
 }
